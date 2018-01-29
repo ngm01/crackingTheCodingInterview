fa23d9f82ff7774af83d1521547cd26e544a3be6
ngm01 <nathaniel.g.moore@gmail.com>
2018-01-28 20:09:07 -0500

    added checkPermutation()

--- JavaScript/ArraysAndStrings.js
+++ JavaScript/ArraysAndStrings.js
@@ -1,4 +1,5 @@
-// 1. Is Unique - implement an algorithm to determine whether a string contains all unqiue characters.
+BasicSearchesAndSorts = require('./BasicSearchesAndSorts');
+// 1.1 Is Unique - implement an algorithm to determine whether a string contains all unqiue characters.
 // What if you cannot use additional data structures?
 function isUnique(str){
     // e.g. 'abcdefgeh' => false
@@ -29,5 +30,46 @@ function isUniqueStringsOnly(str){
 
 }
 
-console.log(isUnique('bar'));
-console.log(isUnique('foo'));
+// console.log(isUnique('bar'));
+// console.log(isUnique('foo'));
+
+// 1.2 Check Permutuation: given two strings, write a method to decide if one is a permutation of the other.
+function checkPermutation(aStr, bStr){
+    if (aStr.length != bStr.length){ 
+        console.log(bStr, "is not a permutation of", aStr);
+        return false;
+    };
+
+    if(compareStrings(aStr, bStr)){
+        console.log(bStr, "is a permutation of", aStr);
+        return true;
+    }
+    else{
+        console.log(bStr, "is not a permutation of", aStr);
+        return false;
+    }
+
+    function compareStrings(aStr, bStr){
+        aStr = splitSortJoin(aStr);
+        bStr = splitSortJoin(bStr);
+        for(i=0; i<aStr.length; i++){
+            if(aStr[i] != bStr[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function splitSortJoin(str){
+        str = str.split('');
+        
+        BasicSearchesAndSorts.quickSort(str);
+        str = str.join('');
+        return str;
+    }
+
+}
+
+checkPermutation("blah", "foobar");
+checkPermutation('blah', 'halb');
+checkPermutation('blah', 'half');
--- JavaScript/BasicSearchesAndSorts.js
+++ JavaScript/BasicSearchesAndSorts.js
@@ -1,149 +1,150 @@
-var myArr = [3, 99, 50, 101, -3, 7];
+module.exports = {
+    bubbleSort: function (arr){
+        //TODO
+    },
 
-function bubbleSort(arr){
-    //TODO
-}
-
-function selectionSort(arr){
-    for(let i=0; i< arr.length - 1; i++){
-        let min = i;
-        for(let k=i+1; k< arr.length; k++){
-            if(arr[k] < arr[min]){
-                let temp = arr[k];
-                arr[k] = arr[i];
-                arr[i] = temp;
+    selectionSort: function (arr){
+        for(let i=0; i< arr.length - 1; i++){
+            let min = i;
+            for(let k=i+1; k< arr.length; k++){
+                if(arr[k] < arr[min]){
+                    let temp = arr[k];
+                    arr[k] = arr[i];
+                    arr[i] = temp;
+                }
             }
         }
-    }
-    return arr;
-}
-
-function insertionSort(arr){
-    //TODO
-}
-
-function shellSort(arr){
-    //TODO
-}
-
-function mergeSort(arr){
-    if (arr.length > 1){
-        let mid = Math.floor(arr.length/2);
-        let leftSide = arr.slice(0, mid);
-        let rightSide = arr.slice(mid, arr.length);
-        return merge(mergeSort(leftSide), mergeSort(rightSide));
-    }
-    else{
         return arr;
-    }
-    function merge(left, right){
-        let newArr = [];
-        let r = 0;
-        let l = 0;
-        while(l < left.length && r < right.length){
-            if(left[l] < right[r]){
-                newArr.push(left[l]);
-                l++;
-            }
-            else{
-                newArr.push(right[r]);
-                r++;
-            }
+    },
+
+    insertionSort: function (arr){
+        //TODO
+    },
+
+    shellSort: function (arr){
+        //TODO
+    },
+
+    mergeSort: function(arr){
+        if (arr.length > 1){
+            let mid = Math.floor(arr.length/2);
+            let leftSide = arr.slice(0, mid);
+            let rightSide = arr.slice(mid, arr.length);
+            return merge(module.exports.mergeSort(leftSide), module.exports.mergeSort(rightSide));
         }
-        newArr = newArr.concat(right.slice(r, right.length));
-        newArr = newArr.concat(left.slice(l, left.length));
-        return newArr;
-    }
-}
+        else{
+            return arr;
+        }
+        function merge(left, right){
+            let newArr = [];
+            let r = 0;
+            let l = 0;
+            while(l < left.length && r < right.length){
+                if(left[l] < right[r]){
+                    newArr.push(left[l]);
+                    l++;
+                }
+                else{
+                    newArr.push(right[r]);
+                    r++;
+                }
+            }
+            newArr = newArr.concat(right.slice(r, right.length));
+            newArr = newArr.concat(left.slice(l, left.length));
+            return newArr;
+        }
+    },
 
-function quickSort(arr){
-    quickSortRecursor(arr, 0, arr.length - 1);
+    quickSort: function(arr){
+        quickSortRecursor(arr, 0, arr.length - 1);
 
-    function quickSortRecursor(arr, begin, end){
+        function quickSortRecursor(arr, begin, end){
 
-        if(begin < end){
+            if(begin < end){
 
-            var divideHere = divideAndConquer(arr, begin, end);
+                divideHere = divideAndConquer(arr, begin, end);
 
-            quickSortRecursor(arr, begin, divideHere-1);
-            quickSortRecursor(arr, divideHere+1, end);
+                quickSortRecursor(arr, begin, divideHere-1);
+                quickSortRecursor(arr, divideHere+1, end);
+            }
+
         }
 
-    }
+        function divideAndConquer(arr, begin, end){
 
-    function divideAndConquer(arr, begin, end){
+            var pivot = arr[begin];
 
-        var pivot = arr[begin];
+            var leftIdx = begin + 1;
+            var rightIdx = end;
 
-        var leftIdx = begin + 1;
-        var rightIdx = end;
+            var indicesHaveCrossed = false;
 
-        var indicesHaveCrossed = false;
+            while(!indicesHaveCrossed){
 
-        while(!indicesHaveCrossed){
+                while(leftIdx <= rightIdx && arr[leftIdx] <= pivot){
+                    leftIdx++;
+                }
+                while(rightIdx >= leftIdx && arr[rightIdx] >= pivot){
+                    rightIdx--;
+                }
 
-            while(leftIdx <= rightIdx && arr[leftIdx] <= pivot){
-                leftIdx++;
-            }
-            while(rightIdx >= leftIdx && arr[rightIdx] >= pivot){
-                rightIdx--;
-            }
-
-            if(rightIdx < leftIdx){
-                indicesHaveCrossed = true;
-            }
-            else{
-                let temp = arr[leftIdx];
-                arr[leftIdx] = arr[rightIdx];
-                arr[rightIdx] = temp;
-    
-            }
-        }
+                if(rightIdx < leftIdx){
+                    indicesHaveCrossed = true;
+                }
+                else{
+                    let temp = arr[leftIdx];
+                    arr[leftIdx] = arr[rightIdx];
+                    arr[rightIdx] = temp;
         
-        let temp = arr[begin];
-        arr[begin] = arr[rightIdx];
-        arr[rightIdx] = temp;
+                }
+            }
+            
+            let temp = arr[begin];
+            arr[begin] = arr[rightIdx];
+            arr[rightIdx] = temp;
 
-        return rightIdx;
+            return rightIdx;
+        }
+    },
+
+    binarySearch: function(arr, sought, begin=0, end=arr.length - 1, mid=arr.length/2){
+        mid = Math.floor(mid);
+        if(arr[mid]==sought){
+            console.log("Found at index:", mid);
+            return mid;
+        }
+        else if(arr[mid]>sought){
+            if(mid == end){
+                console.log("Not found");
+                return -1;
+            }
+            else{
+                // console.log("Searching left, mid is now:", Math.floor((mid + begin)/2));
+                module.exports.binarySearch(arr, sought, begin, mid, (mid + begin)/2)
+            }
+        }
+        else{
+            if(mid==begin){
+                console.log("Not found");
+                return -1;
+            }
+            else{
+                // console.log("Searching right, mid is now:", Math.floor((mid + end)/2));
+                module.exports.binarySearch(arr, sought, mid, end, (mid + end)/2)
+            }
+        }
     }
 }
 
-function binarySearch(arr, sought, begin=0, end=arr.length - 1, mid=arr.length/2){
-    mid = Math.floor(mid);
-    if(arr[mid]==sought){
-        console.log("Found at index:", mid);
-        return mid;
-    }
-    else if(arr[mid]>sought){
-        if(mid == end){
-            console.log("Not found");
-            return -1;
-        }
-        else{
-            // console.log("Searching left, mid is now:", Math.floor((mid + begin)/2));
-            binarySearch(arr, sought, begin, mid, (mid + begin)/2)
-        }
-    }
-    else{
-        if(mid==begin){
-            console.log("Not found");
-            return -1;
-        }
-        else{
-            // console.log("Searching right, mid is now:", Math.floor((mid + end)/2));
-            binarySearch(arr, sought, mid, end, (mid + end)/2)
-        }
-    }
-}
-
-console.log("Unsorted: " + myArr);
-quickSort(myArr);
-console.log("Sorted: " + myArr);
-//Search for elements that are in the array
-binarySearch(myArr, -3);
-binarySearch(myArr, 99);
-//Search for elements that aren't
-binarySearch(myArr, 2);
-binarySearch(myArr, 88);
-binarySearch(myArr, -3);
-binarySearch(myArr, 2);
+// var myArr = [3, 99, 50, 101, -3, 7];
+// console.log("Unsorted: " + myArr);
+// module.exports.quickSort(myArr);
+// console.log("Sorted: " + myArr);
+// //Search for elements that are in the array
+// module.exports.binarySearch(myArr, -3);
+// module.exports.binarySearch(myArr, 99);
+// //Search for elements that aren't
+// module.exports.binarySearch(myArr, 2);
+// module.exports.binarySearch(myArr, 88);
+// module.exports.binarySearch(myArr, -3);
+// module.exports.binarySearch(myArr, 2);
